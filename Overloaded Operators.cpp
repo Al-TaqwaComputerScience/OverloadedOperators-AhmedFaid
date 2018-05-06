6a139baed22471fc8fe7b795b0ea496e760fc0fb
StringName-Faid <38296333+StringName-Faid@users.noreply.github.com>
2018-05-06 01:04:36 -0400

    Add files via upload

--- Overloaded Operators.cpp
+++ Overloaded Operators.cpp
@@ -13,20 +13,38 @@ Coords operator= (Coords rhs){
         this->x = rhs.x;
         this->y = rhs.y;
     }
+
+//Increases y value of coord by one
+Coords operator++ (){
+    this->y++;
+
+    return *this;
+    }
+
+//Decreases x value of coord by one
+Coords operator-- (){
+    if(this->y > 0)
+    this->y--;
+
+    return *this;
+}
+
 };
 
 //Overloaded Coords addition operator
-float operator+ (Coords lhs, Coords rhs){
-    float z;
-    z = sqrt(pow(rhs.x - lhs.x ,2) + pow(rhs.y - lhs.x, 2));
+Coords operator+ (Coords lhs, Coords rhs){
+    Coords z;
+    z.x = lhs.x + rhs.x;
+    z.y = lhs.y + rhs.y;
 
     return z;
 }
 
 //Overloaded Coords subtraction operator
-float operator- (Coords lhs, Coords rhs){
-    float z;
-    z = sqrt(pow(rhs.x - lhs.x ,2) + pow(rhs.x - lhs.x ,2));
+Coords operator- (Coords lhs, Coords rhs){
+    Coords z;
+    z.x = lhs.x - rhs.x;
+    z.y = lhs.y - rhs.y;
 
     return z;
 }
@@ -40,7 +58,8 @@ Coords operator++ (Coords coord){
 
 //Decreases x value of coord by one
 Coords operator-- (Coords coord){
-    coord.x++;
+    if(coord.x > 0)
+    coord.x--;
 
     return coord;
 }
@@ -58,6 +77,64 @@ std::istream& operator>>(std::istream &input, Coords& coord){
 
     input>> coord.x >> coord.y;
     return input;
+}
+
+Coords operator* (int num, Coords coord){
+    Coords newCoord;
+
+    newCoord.x = num*coord.x;
+    newCoord.y = num*coord.y;
+
+    return newCoord;
+}
+
+int operator* (Coords lhs, Coords rhs){
+    int dotProduct;
+    dotProduct = lhs.x*rhs.x + lhs.y*rhs.y;
+
+    return dotProduct;
+}
+
+bool operator < (Coords &coord, const Coords rhs){
+        float mag1, mag2;
+        mag1 = sqrt(pow(coord.x, 2)+pow(coord.y, 2));
+        mag2 = sqrt(pow(rhs.x, 2)+pow(rhs.y, 2));
+
+        if(mag1 < mag2)
+            return true;
+
+        if(mag1 > mag2)
+            return false;
+
+        if(mag1 == mag2)
+            return false;
+
+}
+
+bool operator > (Coords &coord, const Coords rhs){
+    float mag1, mag2;
+    mag1 = sqrt(pow(coord.x, 2)+ pow(coord.y, 2));
+    mag2 = sqrt(pow(rhs.x, 2) + pow(rhs.y, 2));
+
+    if(mag1 > mag2)
+        return true;
+
+    else
+        return false;
+
+}
+
+bool operator == (Coords &coord, const Coords rhs){
+    float mag1, mag2;
+    mag1 = sqrt(pow(coord.x, 2) + pow(coord.y, 2));
+    mag2 = sqrt(pow(rhs.x, 2) + pow(rhs.y, 2));
+
+    if(mag1 == mag2)
+        return true;
+
+    else
+        return false;
+
 }
 
 int main(){
